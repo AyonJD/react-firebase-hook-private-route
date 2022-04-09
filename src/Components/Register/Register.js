a45aa0dce3140c52f14d4fe517fb266970027741
Ayon <ayonjodder177@gmail.com>
2022-04-09 14:21:24 +0600

    Register form added

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -2,8 +2,36 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            Register
+        <div className='w-1/3 mx-auto mt-24 bg-white p-10 bg-opacity-10 bgc-blur bg-clip-padding rounded-lg'>
+
+            <form>
+                <div className="grid xl:grid-cols-2 xl:gap-6">
+                    <div className="relative z-0 mb-6 w-full group">
+                        <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-pink-600 focus:outline-none focus:ring-0 focus:border-pink-500 peer" placeholder=" " required="" />
+                        <label htmlFor="floating_first_name" className="absolute text-sm text-gray-100 dark:text-pink-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pink-300 peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+                    </div>
+                    <div className="relative z-0 mb-6 w-full group">
+                        <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-pink-600 focus:outline-none focus:ring-0 focus:border-pink-500 peer" placeholder=" " required="" />
+                        <label htmlFor="floating_last_name" className="absolute text-sm text-gray-100 dark:text-pink-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pink-300 peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+                    </div>
+                </div>
+                <div className="relative z-0 mb-6 w-full group">
+                    <input type="email" name="floating_email" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-pink-600 focus:outline-none focus:ring-0 focus:border-pink-500 peer" placeholder=" " required="" />
+                    <label htmlFor="floating_email" className="absolute text-sm text-gray-100 dark:text-pink-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pink-300 peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                </div>
+                <div className="relative z-0 mb-6 w-full group">
+                    <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-pink-600 focus:outline-none focus:ring-0 focus:border-pink-500 peer" placeholder=" " required="" />
+                    <label htmlFor="floating_password" className="absolute text-sm text-gray-100 dark:text-pink-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pink-300 peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                </div>
+                <div className="relative z-0 mb-6 w-full group">
+                    <input type="password" name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-pink-600 focus:outline-none focus:ring-0 focus:border-pink-500 peer" placeholder=" " required="" />
+                    <label htmlFor="floating_repeat_password" className="absolute text-sm text-gray-100 dark:text-pink-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pink-300 peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
+                </div>
+                <div className="text-center">
+                    <button type="submit" className="text-white bg-[#F700CC] hover:bg-[#FF0087] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto md:w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+                </div>
+            </form>
+
         </div>
     );
 };
